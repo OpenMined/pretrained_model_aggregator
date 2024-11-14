92947dee7aad30618450af5e85013e31d2b5b06e
Ionésio Junior <ionesiojr@gmail.com>
2024-11-14 17:29:43 -0300

    Update run.sh
    - Remove syftbox installation from if scope
    - Replace uv run python3 main.py -> python3 main.py

--- run.sh
+++ run.sh
@@ -5,17 +5,19 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv -p 3.12 .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox torch --quiet
+    uv pip install -U torch --quiet
     # uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
 fi
 
+uv pip install -U syftbox --quiet
+
 . .venv/bin/activate
 
 # # run app using python from venv
 echo "Running model_aggregator_app with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+python3 main.py
 
 # # deactivate the virtual environment
 deactivate
