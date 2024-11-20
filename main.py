4a39c265449e94b6a21281864e5da6eb248a1867
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-11-20 17:20:45 +0530

    modified the aggregator logic to include the launch, running and done folders move test dataset to samples folder create a private folder for automatically copying the test dataset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 from syftbox.lib import Client
 import os
 import torch
@@ -7,6 +8,21 @@ from torch.utils.data import DataLoader, TensorDataset
 import re
 import json
 
+
+# Exception name to indicate the state cannot advance
+# as there are some pre-requisites that are not met
+class StateNotReady(Exception):
+    pass
+
+APP_NAME = "pretrained_model_aggregator"
+TEST_DATASET_NAME = "mnist_dataset.pt"
+SAMPLE_TEST_DATASET_PATH = Path("samples") / TEST_DATASET_NAME
+
+def get_app_private_data(client: Client, app_name: str) -> Path:
+    """
+    Returns the private data directory of the app
+    """
+    return client.workspace.data_dir / "private" / app_name
 
 class SimpleNN(nn.Module):
     def __init__(self):
@@ -33,8 +49,28 @@ def get_model_file(path: Path):
     return model_files[0] if model_files else None
 
 
+def evaluate_global_model(global_model: nn.Module, dataset_path: Path) -> float:
+    global_model.eval()
+    # load the saved mnist subset
+    images, labels = torch.load(str(dataset_path), weights_only=True)
+    # create a tensordataset
+    dataset = TensorDataset(images, labels)
+    # create a dataloader for the dataset
+    data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+    
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in data_loader:
+            outputs = global_model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct / total
+    return accuracy
+
 def aggregate_model(
-    participants: list[str], datasite_path: Path, global_model_path: Path
+    client: Client,participants: list[str], model_output_path: Path
 ):
     global_model = SimpleNN()
     global_model_state_dict = global_model.state_dict()
@@ -43,16 +79,18 @@ def aggregate_model(
 
     n_peers = len(participants)
     aggregated_peers = []
+    missing_peers = []
     for user_folder in participants:
-        public_folder_path: Path = Path(datasite_path) / user_folder / "public"
+        public_folder_path: Path = client.datasites / user_folder / "public"
 
         model_file = get_model_file(public_folder_path)
         if model_file is None:
+            missing_peers.append(user_folder)
             continue
         model_file = public_folder_path / model_file
         aggregated_peers.append(user_folder)
 
-        user_model_state = torch.load(str(model_file))
+        user_model_state = torch.load(str(model_file), weights_only=True)
         for key in global_model_state_dict.keys():
             # If user model has a different architecture than my global model.
             # Skip it
@@ -65,83 +103,120 @@ def aggregate_model(
                 aggregated_model_weights[key] += user_model_state[key] * (1 / n_peers)
 
     if aggregated_model_weights:
-        print(f"Aggregated models from {aggregated_peers}")
         global_model.load_state_dict(aggregated_model_weights)
-        torch.save(global_model.state_dict(), str(global_model_path))
-        return global_model
+        torch.save(global_model.state_dict(), str(model_output_path))
+        return global_model, missing_peers
     else:
-        return None
+        return None, None
 
 
-def network_participants(datasite_path: Path, participants_json_file_path: Path):
-    exclude_dir = ["apps", ".syft"]
-    entries = os.listdir(datasite_path)
-    with open(participants_json_file_path, "r") as f:
-        participants = json.load(f)
-        participants = participants["participants"]
+def advance_pretrained_aggregator(client: Client) -> None:
+    """
+    Iterates over the running folder and tries to advance it
+    It loads in the participants.json file and aggregates the models
+    """
+    running_folder = client.api_data(APP_NAME) / "running"
+    participants_json = running_folder / "participants.json"
 
-    all_users = []
-    for entry in entries:
-        user_path = Path(datasite_path / entry)
-        is_excluded_dir = entry in exclude_dir
-        is_valid_peer = user_path.is_dir() and not is_excluded_dir
-        if is_valid_peer:
-            all_users.append(entry)
+    if not participants_json.is_file():
+        print("participants.json file not found in the running folder")
+        return
+    
+    with open(participants_json, "r") as f:
+        participants = json.load(f)["participants"]
 
-    participants = list(set(participants) & set(all_users))
-    missing_participants = list(set(participants) - set(all_users))
-    print(f"Pretrained model aggregator participants: {participants}")
-    return participants
+    model_output_path = running_folder / "global_model.pt"
+    
+    global_model, missing_peers = aggregate_model(
+        client,
+        participants,
+        model_output_path
+    )
+
+    if not global_model:
+        print("No models found to aggregate in participants:", participants)
+        return
+    
+    # Evaluate the global model
+    test_dataset_path = get_app_private_data(client, APP_NAME) / TEST_DATASET_NAME
+    accuracy = evaluate_global_model(global_model, test_dataset_path)
+
+    # Write the accuracy to an results.json file
+    results = {
+        "accuracy": accuracy,
+        "participants": participants,
+        "missing_peers": missing_peers
+    }
+    print("Accuracy Results:", results)
+    with open(running_folder / "results.json", "w") as f:
+        json.dump(results, f, indent=4)
+
+    # If no missing peers, move the global model and results.json to the done folder
+    done_folder = client.api_data(APP_NAME) / "done"
+    if not missing_peers:
+        shutil.move(participants_json, done_folder)
+        shutil.move(model_output_path, done_folder)
+        shutil.move(running_folder / "results.json", done_folder)
 
 
-def evaluate_global_model(global_model: nn.Module, dataset_path: Path) -> float:
-    global_model.eval()
-    # load the saved mnist subset
-    images, labels = torch.load(str(dataset_path))
-    # create a tensordataset
-    dataset = TensorDataset(images, labels)
-    # create a dataloader for the dataset
-    data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
-    # dataset = torch.load(str(dataset_path))
-    # data_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=False)
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for images, labels in data_loader:
-            outputs = global_model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = 100 * correct / total
-    return accuracy
+def launch_pretrained_aggregator(client: Client) -> None:
+    """
+    Iterates over the launch folder and copies the participants.json file
+    to the running folder
+
+    We look for the participants.json file in the launch folder
+    """
+    launch_folder = client.api_data(APP_NAME) / "launch"
+    running_folder = client.api_data(APP_NAME) / "running"
+
+    participants_json = launch_folder / "participants.json"
+    if participants_json.is_file():
+        print("Copying participants.json to running folder")
+        shutil.move(participants_json, running_folder)
+    
+
+
+
+
+def init_pretrained_aggregator_app(client: Client) -> None:
+    """
+    Creates the `pretrained_aggregator` app in the `api_data` folder
+    with the following structure:
+    ```
+    api_data
+    └── pretrained_aggregator
+            └── launch
+            └── running
+            └── done
+    ```
+    """
+    pretrained_aggregator = client.api_data(APP_NAME)
+
+    for folder in ["launch", "running", "done"]:
+        pretrained_aggregator_folder = pretrained_aggregator / folder
+        pretrained_aggregator_folder.mkdir(parents=True, exist_ok=True)
+
+    # Create the private data directory for the app
+    # This is where the private test data will be stored
+    app_pvt_dir = get_app_private_data(client, APP_NAME)
+    app_pvt_dir.mkdir(parents=True, exist_ok=True)
+
+    # Copy the test dataset to the private data directory
+    test_dataset_path = app_pvt_dir / TEST_DATASET_NAME
+    if not test_dataset_path.is_file():
+        shutil.copy(SAMPLE_TEST_DATASET_PATH, test_dataset_path)
+
 
 
 if __name__ == "__main__":
     client = Client.load()
-    participants = network_participants(
-        client.datasite_path.parent, Path("participants.json")
-    )
-    global_model = None
-    global_model = aggregate_model(
-        participants,
-        client.datasite_path.parent,
-        client.datasite_path / "public" / "global_model.pth",
-    )
-    if global_model:
-        dataset_path = "./mnist_dataset.pt"
-        accuracy = evaluate_global_model(global_model, dataset_path)
-        print(f"Global model accuracy: {accuracy:.2f}%")
-    else:
-        print("No models to aggregate")
 
-    output_dir: Path = client.api_data("model_aggregator")
+    # Step 1: Init the Pretrained Aggregator App
+    init_pretrained_aggregator_app(client)
 
-    if not output_dir.is_dir():
-        os.mkdir(str(output_dir))
+    # Step 2: Launch the App
+    launch_pretrained_aggregator(client)
 
-    with open(str(output_dir) + "/results.json", "w") as json_file:
-        json.dump(
-            {"accuracy": accuracy, "participants": participants},
-            json_file,
-            indent=4,
-        )
+    # Step 3: Advance the Pretrained Aggregator App
+    # Iterates over the running folder and tries to advance it.
+    advance_pretrained_aggregator(client)
