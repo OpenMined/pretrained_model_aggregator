1c0d662d4c512b1b17f9d0d07184cf0e9c6fa15d
Yash Gorana <yash.gorana@hotmail.com>
2025-04-08 19:28:10 +0530

    fix 10s loop

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
+
 set -e
 
-if [ ! -d ".venv" ]; then
-    echo "Virtual environment not found. Creating one..."
-    uv venv -p 3.12 .venv
-    echo "Virtual environment created successfully."
-else
-    echo "Virtual environment already exists."
-fi
-
+uv venv -p 3.12 .venv
+uv pip install -U -r requirements.txt
 . .venv/bin/activate
 
-uv pip install -U syft-core torch --quiet
+while true; do
+    echo "Running 'pretrained_model_aggregator' with $(python3 --version) at '$(which python3)'"
+    python3 main.py
 
-# run app using python from venv
-echo "Running model_aggregator_app with $(python3 --version) at '$(which python3)'"
-python3 main.py
+    echo "Sleeping for 10 seconds..."
+    sleep 10
+done
 
-# deactivate the virtual environment
 deactivate
